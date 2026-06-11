31a832897c162d3af266d948ad46a523ed32fe7d
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-06-11 21:03:32 +0300

    PDF image conversion & AI feedback

---
+++ app/lib/utils.ts
@@ -0,0 +1,15 @@
+@params bytes
+@returns
+
+export function formatSize(bytes:number): string {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat(bytes/Math.pow(k,i)).toFixed(2)) + ' ' + sizes[]
+}
+
+export const generateUUID = () => crypto.randomUUID();
